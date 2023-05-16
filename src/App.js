ef58a57df126dd0b944d6bea9236e4108c8d4360
alinamikulska <alinamikulska@mail.ru>
2023-05-16 23:45:33 +0300

    final

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<Form />}/>
-                <Route path={'product'} element={<ProductList />}/>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
             </Routes>
         </div>
     );
