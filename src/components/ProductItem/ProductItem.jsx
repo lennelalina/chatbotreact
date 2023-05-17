711e289bd201faba3d3b5349d50bbb5e7fa3dece
alinamikulska <alinamikulska@mail.ru>
2023-05-17 14:12:25 +0300

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,18 +3,18 @@ import Button from "../Button/Button";
 import './ProductItem.css';
 
 const ProductItem = ({product, className, onAdd}) => {
-
+const {title, Image, description,price} = product;
     const onAddHandler = () => {
         onAdd(product);
     }
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
-            <div className={'title'}>{product.title}</div>
-            <div className={'description'}>{product.description}</div>
+            <div className={'Image'}>  <img src={Image} width={'100%'}/> </div>
+            <div className={'title'}>{title}</div>
+            <div className={'description'}>{description}</div>
             <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+                <span>Стоимость: <b>{price}</b></span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
